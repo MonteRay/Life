c77f970644d0d64789ed3c11922bef5e510fb926
MonteRay <areagle@mail.ru>
2015-05-13 20:40:48 +0300

    123

---
+++ Life/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace Life
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.nudScale = new System.Windows.Forms.NumericUpDown();
+            this.startStopBtn = new System.Windows.Forms.Button();
+            this.BtnGen = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tmr = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudScale)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.numericUpDown2);
+            this.panel1.Controls.Add(this.nudScale);
+            this.panel1.Controls.Add(this.startStopBtn);
+            this.panel1.Controls.Add(this.BtnGen);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(101, 230);
+            this.panel1.TabIndex = 3;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(12, 96);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            25,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(75, 20);
+            this.numericUpDown2.TabIndex = 6;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // nudScale
+            // 
+            this.nudScale.Location = new System.Drawing.Point(12, 12);
+            this.nudScale.Maximum = new decimal(new int[] {
+            25,
+            0,
+            0,
+            0});
+            this.nudScale.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudScale.Name = "nudScale";
+            this.nudScale.Size = new System.Drawing.Size(75, 20);
+            this.nudScale.TabIndex = 5;
+            this.nudScale.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // startStopBtn
+            // 
+            this.startStopBtn.Location = new System.Drawing.Point(12, 67);
+            this.startStopBtn.Name = "startStopBtn";
+            this.startStopBtn.Size = new System.Drawing.Size(75, 23);
+            this.startStopBtn.TabIndex = 4;
+            this.startStopBtn.Text = "Start/Stop";
+            this.startStopBtn.UseVisualStyleBackColor = true;
+            this.startStopBtn.Click += new System.EventHandler(this.startStopBtn_Click);
+            // 
+            // BtnGen
+            // 
+            this.BtnGen.Location = new System.Drawing.Point(12, 38);
+            this.BtnGen.Name = "BtnGen";
+            this.BtnGen.Size = new System.Drawing.Size(75, 23);
+            this.BtnGen.TabIndex = 3;
+            this.BtnGen.Text = "Generate";
+            this.BtnGen.UseVisualStyleBackColor = true;
+            this.BtnGen.Click += new System.EventHandler(this.BtnGen_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(107, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(396, 230);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // tmr
+            // 
+            this.tmr.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(503, 230);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.MinimumSize = new System.Drawing.Size(200, 150);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudScale)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.NumericUpDown nudScale;
+        private System.Windows.Forms.Button startStopBtn;
+        private System.Windows.Forms.Button BtnGen;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer tmr;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+
+    }
+}
+
---
+++ Life/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Life
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public bool[,] genField(int width, int height, int fillFactor)
+        {
+            int cc;
+            Random rnd = new Random();
+            bool[,] result = new bool[width, height];
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    cc = rnd.Next(101);
+                    if (cc > fillFactor)
+                    {
+                        result[i,j]=true;
+                    }
+                    else
+                    {
+                        result[i,j]=false;
+                    }
+
+
+                }
+            }
+            return result;
+        }
+
+        public Bitmap drawField(bool[,] field, int scaleFactor)
+        {
+            int width = field.GetLength(0);
+            int height = field.GetLength(1);
+            Bitmap result = new Bitmap(width*scaleFactor, height*scaleFactor);
+            System.Drawing.Color color;
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    if (field[x,y])
+                    {
+                        color = System.Drawing.Color.Black;
+                    }
+                    else
+                    {
+                        color = System.Drawing.Color.White;
+                    }
+
+                    for (int dx = 0; dx < scaleFactor; dx++)
+                        for (int dy = 0; dy < scaleFactor; dy++)
+                        {
+                            result.SetPixel(x*scaleFactor+dx, y*scaleFactor+dy, color);
+                        }
+                        
+
+                }
+            }
+            return result;
+        }
+        //public class tField:
+        //   public bool genField()
+        // {
+        //
+        // }
+        public static class Glob
+        {
+            public static bool[,] field;
+        }
+
+
+        private void BtnGen_Click(object sender, EventArgs e)
+        {
+            
+
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            int width = pictureBox1.Width;
+            int height = pictureBox1.Height;
+            int fillFactor = 30;
+           
+            int scaleFactor = decimal.ToInt32(nudScale.Value);
+
+            int scaledWidth = width/scaleFactor;
+            int scaledHeight = height/scaleFactor;
+            
+            Glob.field = genField(scaledWidth, scaledHeight, fillFactor);
+
+            pictureBox1.Image = drawField(Glob.field,scaleFactor);
+
+        }
+
+        private void startStopBtn_Click(object sender, EventArgs e)
+        {
+            tmr.Enabled = !tmr.Enabled;
+        }
+    }
+
+    public class node
+    {
+        public int id { get; set; }
+        public int age { get; set; }
+    }
+
+    public class universe
+    {
+
+    }
+
+    
+
+}
